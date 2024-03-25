6df53269993cbbb3f87720d0017b03abadb1997c
Enes Eken <eneseken@Enes-MacBook-Air.local>
2024-03-26 01:01:42 +0300

    Swift

---
+++ Swift/SwiftfulThinkingBasicsBootcamp.playground/Pages/BasicTypes.xcplaygroundpage/Contents.swift
@@ -0,0 +1,25 @@
+import UIKit
+
+var greeting = "Hello, playground"
+
+let firstGreeting: String = "Hello"
+
+// Reference to previously created variable
+let myTitle = greeting
+
+
+// Boolen true/false
+let mySecondItem = true
+
+
+// Date
+let myFirstDate: Date = Date() 
+
+
+// Numbers
+
+let myNumbers = 1
+let mySecondNumbers = 2.1
+
+let numbers: Double = 3.5
+
---
+++ Swift/SwiftfulThinkingBasicsBootcamp.playground/Pages/SwiftBasics.xcplaygroundpage/Contents.swift
@@ -0,0 +1,22 @@
+import Foundation
+
+var greeting = "Hello, playground"
+
+print(greeting)
+
+// Hello Swift!
+
+/* Swift */
+
+
+// Camel Case - right
+
+let firstGreeting = "Hello swift!"
+
+// Snake Case - wrong
+
+let first_greeting = "Hello"
+
+// Pascal Case - wrong
+
+let FirstGreeting = "Hello"
