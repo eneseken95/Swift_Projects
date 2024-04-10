01abeabf68474c8276f0ef6556ac9f6df45672a8
Enes Eken <eneseken@Enes-MacBook-Air.local>
2024-04-10 13:05:55 +0300

    Update

--- SwiftPlayground/SwiftfulThinkingBasicsBootcamp.playground/Pages/Protocols.xcplaygroundpage/Contents.swift
+++ SwiftPlayground/SwiftfulThinkingBasicsBootcamp.playground/Pages/Protocols.xcplaygroundpage/Contents.swift
@@ -1,11 +1,68 @@
 import Foundation
 
-struct EmployeeModel: EmployeeModelName {
-    let title: String
-    let name: String
+protocol Vehicle 
+{
+    var numberOfWheels: Int { get }
+    func startEngine()
+    func stopEngine()
 }
 
-protocol EmployeeModelName
+class Car: Vehicle 
 {
-    let name: String
+    var numberOfWheels: Int = 4
+    
+    func startEngine() 
+    {
+        print("Car engine started")
+    }
+    
+    func stopEngine() 
+    {
+        print("Car engine stopped")
+    }
 }
+
+class Truck: Vehicle 
+{
+    var numberOfWheels: Int = 6
+    
+    func startEngine()
+    {
+        print("Truck engine started")
+    }
+    
+    func stopEngine() 
+    {
+        print("Truck engine stopped")
+    }
+}
+
+class Bicycle: Vehicle 
+{
+    var numberOfWheels: Int = 2
+    
+    func startEngine() 
+    {
+        print("Bicycle has no engine")
+    }
+    
+    func stopEngine() 
+    {
+        print("Bicycle has no engine")
+    }
+}
+
+let car = Car()
+let truck = Truck()
+let bicycle = Bicycle()
+
+let vehicles: [Vehicle] = [car, truck, bicycle]
+
+for vehicle in vehicles 
+{
+    print("Number of wheels: \(vehicle.numberOfWheels)")
+    vehicle.startEngine()
+    vehicle.stopEngine()
+    print("--------------------")
+}
+
