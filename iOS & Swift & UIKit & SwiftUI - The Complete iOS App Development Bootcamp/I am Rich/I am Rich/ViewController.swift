ef40e654e840f0a66a24628761273e212ef15bf4
Enes Eken <eneseken@172-2-0-170.lightspeed.dybhfl.sbcglobal.net>
2024-07-31 19:19:53 +0300

    Update

---
+++ iOS & Swift & UIKit & SwiftUI - The Complete iOS App Development Bootcamp/I am Rich/I am Rich/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  I am Rich
+//
+//  Created by Enes Eken on 23.07.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
